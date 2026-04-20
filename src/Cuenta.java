b7e72c714b549dc665ae562994f509c3d6dbcd2b
daniperucho <adperucho@gmail.com>
2026-04-20 12:29:30 +0200

    ejemplos finalizados

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -1,5 +1,27 @@
 public class Cuenta {
-    public int getSaldo() {
-        return 0;
+    private double saldo;
+
+    public Cuenta(){
+        saldo = 0.0f;
     }
+    public double getSaldo() {
+        return saldo;
+    }
+    public boolean esCantidadValida(double cantidad){
+        if(cantidad <0 || cantidad >6000) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    public void ingresar(double cantidad){
+        if(!esCantidadValida(cantidad)) {
+            saldo = 0.0f;
+        }
+        else {
+            saldo += cantidad;
+        }
+    }
+
 }
--- test/tddSampleTest.java
+++ test/tddSampleTest.java
@@ -8,5 +8,48 @@ class tddSampleTest {
         Cuenta c = new Cuenta();
         assertEquals(0, c.getSaldo());
     }
+    @Test
+    void testAlIngresar100EnCuentaVaciaElSaldoEs100(){
+        Cuenta c = new Cuenta();
+        c.ingresar(100);
+        assertEquals(100,c.getSaldo());
+    }
+    @Test
+    void testAlIngresar3000EnCuentaVaciaElSaldoEs3000(){
+        Cuenta c = new Cuenta();
+        c.ingresar(3000);
+        assertEquals(3000,c.getSaldo());
+    }
+    @Test
+    void testAlIngresar100EnCuentaCon3000ElSaldoEs3100(){
+        Cuenta c = new Cuenta();
+        c.ingresar(100);
+        c.ingresar(3000);
+        assertEquals(3100,c.getSaldo());
+    }
+    @Test
+    void testNoSePuedeIngresarCAntidadNegativa() {
+        Cuenta c = new Cuenta();
+        c.ingresar(-100);
+        assertEquals(0,c.getSaldo());
+    }
+    @Test
+    void IngresoCantidad2Decimales(){
+        Cuenta c = new Cuenta();
+        c.ingresar(100.45);
+        assertEquals(100.45,c.getSaldo());
+    }
+    @Test
+    void testIngresoMaximoEsDe6000(){
+        Cuenta c = new Cuenta();
+        c.ingresar(6000);
+        assertEquals(6000,c.getSaldo());
+    }
+    @Test
+    void testIngresoMasDe6000NoEsValido(){
+        Cuenta c = new Cuenta();
+        c.ingresar(6000.01);
+        assertEquals(0,c.getSaldo());
+    }
 
 }
